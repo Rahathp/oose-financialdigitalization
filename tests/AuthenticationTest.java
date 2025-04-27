7486026dd4476b275129578eae80d8058c2b6b71
rahath p <136153716+Rahathp@users.noreply.github.com>
2025-04-28 02:15:28 +0300

    Update AuthenticationTest.java

--- tests/AuthenticationTest.java
+++ tests/AuthenticationTest.java
@@ -6,7 +6,6 @@ public class AuthenticationTest {
     @Test
     void testLogin() {
         Authentication authentication = new Authentication();
-        // Similar to Account, since login is not implemented, assert that it throws an exception.
         assertThrows(UnsupportedOperationException.class, authentication::login, "login method not implemented");
     }
 }
