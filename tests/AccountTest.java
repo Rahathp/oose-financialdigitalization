ae7a1753fde26704d87e9837b6e4764e0af1f35c
rahath p <136153716+Rahathp@users.noreply.github.com>
2025-04-28 02:07:44 +0300

    Update AccountTest.java

--- tests/AccountTest.java
+++ tests/AccountTest.java
@@ -6,7 +6,6 @@ public class AccountTest {
     @Test
     void testLinkAccount() {
         Accounts account = new Accounts();
-        // Since linkAccount is not implemented, we assert that it throws an exception.
         assertThrows(UnsupportedOperationException.class, account::linkAccount, "linkAccount method not implemented");
     }
 }
